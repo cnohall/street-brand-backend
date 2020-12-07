4d83f672c34b011b280d734665aa90496009d3f0
Christopher Nohall <christopher.nohall@gmail.com>
2020-12-07 23:12:32 +0900

    kyc route setup

--- src/interfaces/customer.ts
+++ src/interfaces/customer.ts
@@ -1,4 +1,4 @@
-export interface Customer {
+export default interface Customer {
     BirthDate:      string,
     GivenName:      string,
     MiddleName?:     string, // optional
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,39 @@
+//Packages
 import express from 'express';
 import * as dotenv from "dotenv";
+import bodyParser from 'body-parser';
+import axios from 'axios'
+//Interfaces
+import Customer from './interfaces/customer'
+
+
 dotenv.config();
-// rest of the code remains same
+const URL = process.env.API_ENDPOINT || '';
+const axiosConfig = {
+    headers: {
+        'Authorization': 'Bearer ' + process.env.API_KEY,
+        'Content-Type': 'application/json',
+    }
+};
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+app.use(bodyParser.json())
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+app.post('/kyc', (req, res) => {
+    let customer = req.body;
+    axios.post(URL, customer, axiosConfig)
+    .then((result) => {
+        res.json(result.data);
+    })
+    .catch((error)=> {
+        res.json(error);
+    })
+});
+

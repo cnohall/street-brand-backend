4bf86ce282e5791ab1939589907a98c09433f5d1
Christopher Nohall <christopher.nohall@gmail.com>
2020-12-08 16:41:18 +0900

    see if cors is the problem

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import * as dotenv from "dotenv";
 import bodyParser from 'body-parser';
 import axios from 'axios';
-import cors from 'cors';
+// import cors from 'cors';
 
 //Local Files
 import {CustomerValidation} from './validation/customer-validation'
@@ -25,7 +25,7 @@ const axiosConfig = {
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors())
+// app.use(cors())
 
 app.use(bodyParser.json())
 app.listen(PORT, () => {
